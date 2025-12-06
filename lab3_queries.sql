f0ff85681c82099143fea338f3d385ab7bbd560c
Mohammed <mohammedennassiry99@gmail.com>
2025-12-06 20:11:13 +0100

    lab 3

---
+++ lab3_queries.sql
@@ -0,0 +1,64 @@
+SELECT * FROM auteur;
+SELECT id, nom FROM abonne;
+SELECT titre, disponible
+FROM ouvrage
+WHERE disponible = TRUE;
+
+SELECT * 
+FROM emprunt
+WHERE date_debut > '2025-06-01';
+
+SELECT titre
+FROM ouvrage
+WHERE disponible = FALSE
+  AND auteur_id = 3;
+
+  SELECT nom
+FROM abonne
+WHERE email LIKE '%@gmail.com'
+   OR email LIKE '%@yahoo.com';
+
+   SELECT *
+FROM abonne
+WHERE NOT (email LIKE '%@example.com');
+
+SELECT nom
+FROM auteur
+WHERE nom LIKE 'M%';  
+
+SELECT email
+FROM abonne
+WHERE email LIKE '%@%';
+
+SELECT nom
+FROM abonne
+WHERE id IN (1, 3, 5);
+
+SELECT *
+FROM emprunt
+WHERE date_debut BETWEEN '2025-05-01' AND '2025-05-31';
+
+
+SELECT *
+FROM emprunt
+WHERE date_fin IS NULL;
+
+SELECT *
+FROM emprunt
+WHERE date_fin IS NOT NULL;
+
+
+SELECT titre, disponible
+FROM ouvrage
+ORDER BY titre ASC;
+
+SELECT * 
+FROM abonne
+ORDER BY id
+LIMIT 10;
+
+
+SELECT *
+FROM abonne
+ORDER BY id
+LIMIT 5 OFFSET 5;
